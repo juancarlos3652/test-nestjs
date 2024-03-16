2a399055b8bcd4208e1ebc5f5a1dada38d9a68d7
juancarlos3652 <juan.quispem.27@unsch.edu.pe>
2024-03-16 09:01:32 -0500

    getAllTasks controller + service added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  getAllTasks(){
+    return [{id: "i", title: 'first task', description: 'some tasks'}]
+  }
+
+  createtask(){}
+  
+  updateTask(){}
+  
+  deleteTask(){}
+}
