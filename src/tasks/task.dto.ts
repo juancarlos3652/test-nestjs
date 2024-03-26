07fd44f40da41055e502f1310aa1ff0e444d3496
juancarlos3652 <juan.quispem.27@unsch.edu.pe>
2024-03-26 07:38:58 -0500

    add validation for fields(PUT & PATCH)

--- src/tasks/task.dto.ts
+++ src/tasks/task.dto.ts
@@ -1,12 +1,32 @@
 import { TaskStatus } from './task.entity';
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsOptional,
+  IsIn,
+} from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+  @IsString()
   description: string;
 }
 
 export class UpdateFieldsDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsOptional()
   title?: string;
+  @IsString()
+  @IsOptional()
   description?: string;
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.IN_PROGRESS, TaskStatus.PENDING, TaskStatus.DONE])
   status?: TaskStatus;
 }
